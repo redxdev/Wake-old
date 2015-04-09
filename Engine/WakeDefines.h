b0d2168bb7713c363a9be7052cc4fd67431d3d6a
Sam Bloomberg <sam@redxdev.com>
2015-04-09 13:58:47 -0400

    doubles -> floats

--- Engine/WakeDefines.h
+++ Engine/WakeDefines.h
@@ -3,7 +3,7 @@
 #define WAKE_VERSION ("1.0")
 #define WAKE_VERSION_ID (1)
 
-#define WAKE_CLEAR_COLOR 0.392, 0.584, 0.929
+#define WAKE_CLEAR_COLOR 0.392f, 0.584f, 0.929f
 
 #if defined(WAKE_RELEASE)
 #define WAKE_NO_EXIT_PAUSE
