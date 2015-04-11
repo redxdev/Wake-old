c10bc664a36a7e8075681de1bf44f3ba942c8397
Sam Bloomberg <sam@redxdev.com>
2015-04-11 18:22:22 -0400

    fixed up example (new input functions)

--- Engine/Main.cpp
+++ Engine/Main.cpp
@@ -110,28 +110,23 @@ void Setup()
 {
 	W_INPUT.Bind("Exit", INPUT_BIND(Keyboard, Pressed, Escape));
 	W_INPUT.Event("Exit").Bind(&OnInput_Exit);
-	
-	/*W_INPUT.CreateBinding("Exit", INPUT_BIND(Keyboard, Pressed, Escape));
-	W_INPUT.Event("Exit").Bind(&OnInput_Exit);
 
-	W_INPUT.CreateBinding("New", INPUT_BIND(Keyboard, Pressed, Return));
+	W_INPUT.Bind("New", INPUT_BIND(Keyboard, Pressed, Return));
 	W_INPUT.Event("New").Bind(&OnInput_New);
 
-	W_INPUT.CreateBinding("Left", INPUT_BIND(Keyboard, Pressed, Left));
-	W_INPUT.CreateBinding("Left", INPUT_BIND(Keyboard, Released, Left));
-	W_INPUT.CreateBinding("Right", INPUT_BIND(Keyboard, Pressed, Right));
-	W_INPUT.CreateBinding("Right", INPUT_BIND(Keyboard, Released, Right));
-	W_INPUT.CreateBinding("Up", INPUT_BIND(Keyboard, Pressed, Up));
-	W_INPUT.CreateBinding("Up", INPUT_BIND(Keyboard, Released, Up));
-	W_INPUT.CreateBinding("Down", INPUT_BIND(Keyboard, Pressed, Down));
-	W_INPUT.CreateBinding("Down", INPUT_BIND(Keyboard, Released, Down));
-
+	W_INPUT.Bind("Left", INPUT_BIND(Keyboard, Pressed, Left));
+	W_INPUT.Bind("Left", INPUT_BIND(Keyboard, Released, Left));
+	W_INPUT.Bind("Right", INPUT_BIND(Keyboard, Pressed, Right));
+	W_INPUT.Bind("Right", INPUT_BIND(Keyboard, Released, Right));
+	W_INPUT.Bind("Up", INPUT_BIND(Keyboard, Pressed, Up));
+	W_INPUT.Bind("Up", INPUT_BIND(Keyboard, Released, Up));
+	W_INPUT.Bind("Down", INPUT_BIND(Keyboard, Pressed, Down));
+	W_INPUT.Bind("Down", INPUT_BIND(Keyboard, Released, Down));
+	
 	W_INPUT.Event("Left").Bind(&Left);
 	W_INPUT.Event("Right").Bind(&Right);
 	W_INPUT.Event("Up").Bind(&Up);
 	W_INPUT.Event("Down").Bind(&Down);
-
-	W_WORLD.SpawnActor<TestActor>();*/
 }
 
 void RunEngine(int ArgC, char** ArgV)
